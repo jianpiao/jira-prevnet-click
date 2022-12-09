b96e6fd1b4c6a84640f53de39b57c6fcfa7fc64c
GauharChan <45651308+GauharChan@users.noreply.github.com>
2022-12-09 16:06:58 +0800

    perf: 文件名语义化，injectCustomJs函数使用参数默认值

--- js/inject.js
+++ js/inject.js
@@ -1,11 +1,17 @@
-function init() {
-  const dom = document.querySelector("#description-val");
-  if (dom) {
-    dom.setAttribute("style", "pointer-events: none;");
-  }
-}
+console.info("jira-prevent-click is done !!!!");
 
-// 观察节点变化，实时更新节点状态
-var elementToObserve = document.querySelector("body");
-var observer = new MutationObserver(init);
-observer.observe(elementToObserve, { subtree: true, childList: true });
+// content_scripts会自动将内容注入
+document.addEventListener("DOMContentLoaded", function () {
+  injectCustomJs();
+});
+
+// 向页面注入自定义js
+function injectCustomJs(jsPath = "js/prevent.js") {
+  const temp = document.createElement("script");
+  temp.setAttribute("type", "text/javascript");
+  temp.src = chrome.runtime.getURL(jsPath);
+  temp.onload = function () {
+    console.log("jira-prevent-click is loaded！");
+  };
+  document.body.appendChild(temp);
+}
